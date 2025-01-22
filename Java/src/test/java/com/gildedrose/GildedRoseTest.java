eb3fcb0d423627255e63334a0bc1c27da0561fee
BenjaminHoggan <benjamin.hoggan@vector.co.nz>
2025-01-23 10:43:09 +1300

    Refactor test code

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,35 +1,45 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+    private GildedRose sut;
+    private Item[] items;
+
+    @BeforeEach
+    void setUp() {
+        final String itemName = "someString";
+        final int itemSellIn = 1000;
+        final int itemQuality = 0;
+
+        items = new Item[] {
+            new Item(itemName, itemSellIn, itemQuality)
+        };
+
+        sut = new GildedRose(items);
+    }
+
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        // When
+        sut.updateQuality();
+
+        // Then
+        assertEquals("someString", sut.items[0].name);
     }
 
     @Test
     void Does_not_change_quality_if_quality_is_zero() {
         // Given
-        final String itemName = "someString";
-        final int itemSellIn = 1000;
-        final int itemQuality = 0;
-        final Item[] items = new Item[] {
-            new Item(itemName, itemSellIn, itemQuality)
-        };
-        GildedRose app = new GildedRose(items);
-
         // When
-        app.updateQuality();
+        sut.updateQuality();
 
         // Then
-        assertEquals(0, app.items[0].quality);
+        assertEquals(0, sut.items[0].quality);
     }
 
 }

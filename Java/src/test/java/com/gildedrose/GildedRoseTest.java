33ffec437497b380acd6b84fe4a876f679bc2597
BenjaminHoggan <benjamin.hoggan@vector.co.nz>
2025-01-23 10:37:08 +1300

    Test does not change quality if quality is zero

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,22 @@ class GildedRoseTest {
         assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void Does_not_change_quality_if_quality_is_zero() {
+        // Given
+        final String itemName = "someString";
+        final int itemSellIn = 1000;
+        final int itemQuality = 0;
+        final Item[] items = new Item[] {
+            new Item(itemName, itemSellIn, itemQuality)
+        };
+        GildedRose app = new GildedRose(items);
+
+        // When
+        app.updateQuality();
+
+        // Then
+        assertEquals(0, app.items[0].quality);
+    }
+
 }
